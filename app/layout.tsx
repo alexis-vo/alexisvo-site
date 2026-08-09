7d1aa804048f1d7d21364b4bb5c3dd748fbb5d91
alexis-vo <alexisvo.perso@gmail.com>
2026-08-09 15:13:21 +0200

    fix playfair

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import ThemeScript from "@/components/ThemeScript";
 const playfair = Playfair_Display({
   subsets: ["latin"],
   style: ["normal", "italic"],
+  variable: "--font-playfair",
 });
 
 export const metadata: Metadata = {
